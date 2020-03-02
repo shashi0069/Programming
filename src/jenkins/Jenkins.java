4a12684507f272b315bf53d3c56d6ca088dc4086
hp <hp@Shashi-Kumar>
2020-03-02 17:01:25 +0530

    Programming.

---
+++ src/jenkins/Jenkins.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Jenkins 
+{
+    @Test
+    
+    public void Demo() 
+    {
+    	System.out.println("Demo");
+    }
+}
